7f1ad65a269c265cefc13592f020de1563fe0c83
AlicornLunaa <alicornlunaa@gmail.com>
2021-08-04 11:40:42 -0500

    Added API loading

--- startup.lua
+++ startup.lua
@@ -1,2 +1,16 @@
 -- Helios bootloader
-shell.setPath(shell.path() .. ":/bin") -- Allows system programs to be executed anywhere
+print("Helios Bootloader v1.0")
+
+-- Allows system programs to be executed anywhere
+shell.setPath(shell.path() .. ":/bin")
+
+-- Load all the APIs
+for k, v in pairs(fs.list("/lib")) do
+    print("Loading API: " .. v)
+    os.loadAPI(fs.combine("/lib", v))
+end
+
+-- Finish
+sleep(0.25)
+term.setCursorPos(1, 1)
+term.clear()
