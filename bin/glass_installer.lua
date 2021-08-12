fe717f33edc45e0b5b043d4a8e786f0363b471ed
AlicornLunaa <alicornlunaa@gmail.com>
2021-08-12 01:33:06 -0500

    Added package data and created installer install function

--- bin/glass_installer.lua
+++ bin/glass_installer.lua
@@ -1,10 +1,61 @@
 -- Update operating system
 -- Variables
+local masterBranch = "https://raw.githubusercontent.com/AlicornLunaa/glass_os/master"
 local args = { ... }
 
 -- Functions
-function installGlass()
+function checkHttp()
+    -- Checks if the http connection is even valid
+    if not http.checkURL("https://github.com/AlicornLunaa/glass_os") then
+        print("Unable to contact Github.")
+        return false
+    end
 
+    print("Connection to Github ok.")
+    return true
+end
+
+function getHttpData(url)
+    -- Gets a file from HTTP protocol and returns the data
+    local res = http.get(url)
+    local data = res.readAll()
+    res.close()
+
+    return data
+end
+
+function installGlass()
+    -- Get all the files from github
+    print("Installing Glass...")
+    if not checkHttp() then return end
+
+    -- Obtain the package information for which files to download
+    term.write("Getting package details...")
+    local pkg = textutils.unserialiseJSON(getHttpData(masterBranch .. "/package.json"))
+    print(" Done!")
+
+    -- Create directories listed
+    if pkg then
+        print("Creating file structure...")
+        for _,v in pairs(pkg.directories) do
+            print("Creating directory " .. v)
+            fs.makeDir(v)
+        end
+        print("Done!")
+
+        print("Downloading files...")
+        for _,v in pairs(pkg.files) do
+            print("Downloading file " .. v)
+            local data = getHttpData(pkg.masterUrl .. pkg.files)
+            local f = fs.open(v, "w")
+            f.write(data)
+            f.close()
+        end
+        print("Done!")
+    else
+        print("Unknown error!")
+        return
+    end
 end
 
 function updateGlass()
